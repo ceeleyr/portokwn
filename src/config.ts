3cd1941b581b97f9ca16e22c8add4349085f6801
ceeleyr <ceeleyrichela@gmail.com>
2025-12-19 02:26:51 +0700

    niceee

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Portofolio Etika Budaya - Ceeley Richela';
-export const SITE_DESCRIPTION = 'Website portofolio untuk tugas akhir semester mata kuliah Etika Budaya';
+export const SITE_TITLE = 'Portofolio Kewarganegaraan - Ceeley Richela';
+export const SITE_DESCRIPTION = 'Website portofolio untuk tugas akhir semester mata kuliah Kewarganegaraan';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
